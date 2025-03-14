690d815e145ef536670e5b2755cda622b512eedd
z0052jaz <jan.buchmiller@siemens.com>
2025-03-14 13:11:44 +0100

    feat: add console outputs for tests

--- webapi/DynamoDbTests.cs
+++ webapi/DynamoDbTests.cs
@@ -21,18 +21,23 @@ namespace webapi
         [TestMethod]
         public async Task CanCreateAndDeleteItem()
         {
+            Console.WriteLine("Creating test item...");
+            var testItem = new Dictionary<string, AttributeValue>
+            {
+                { "Id", new AttributeValue { S = "123" } },
+                { "Name", new AttributeValue { S = "Test Item" } }
+            };
+
             var putItemRequest = new PutItemRequest
             {
                 TableName = "TestTable",
-                Item = new Dictionary<string, AttributeValue>
-                {
-                    { "Id", new AttributeValue { S = "123" } },
-                    { "Name", new AttributeValue { S = "Test Item" } }
-                }
+                Item = testItem
             };
 
             await _dynamoDbClient.PutItemAsync(putItemRequest);
+            Console.WriteLine($"Created test item: {string.Join(", ", testItem.Select(x => $"{x.Key}={x.Value.S}"))}");
 
+            Console.WriteLine("Retrieving test item...");
             var getItemRequest = new GetItemRequest
             {
                 TableName = "TestTable",
@@ -44,11 +49,14 @@ namespace webapi
 
             var response = await _dynamoDbClient.GetItemAsync(getItemRequest);
             Assert.IsTrue(response.Item.ContainsKey("Name"));
+            Console.WriteLine($"Retrieved test item: {string.Join(", ", response.Item.Select(x => $"{x.Key}={x.Value.S}"))}");
 
+            Console.WriteLine("Deleting test item...");
             await _dynamoDbClient.DeleteItemAsync("TestTable", new Dictionary<string, AttributeValue>
             {
                 { "Id", new AttributeValue { S = "123" } }
             });
+            Console.WriteLine("Test item deleted successfully.");
         }
     }
 }
