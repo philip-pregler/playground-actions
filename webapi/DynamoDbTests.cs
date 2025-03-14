2ed298ee9a61d6b3d55ad77afa75316cf87bd39a
z0052jaz <jan.buchmiller@siemens.com>
2025-03-14 11:40:37 +0100

    feat: try with new project

---
+++ webapi/DynamoDbTests.cs
@@ -0,0 +1,55 @@
+using Amazon.DynamoDBv2;
+using Amazon.DynamoDBv2.Model;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DefaultNamespace;
+
+{
+    [TestClass]
+    public class DynamoDbTests
+    {
+        private AmazonDynamoDBClient _dynamoDbClient;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _dynamoDbClient = new AmazonDynamoDBClient(new AmazonDynamoDBConfig
+            {
+                ServiceURL = Environment.GetEnvironment("DYNAMODB_ENDPOINT")
+            });
+        }
+
+        [TestMethod]
+        public async Task CanCreateAndDeleteItem()
+        {
+            var putItemRequest = new PutItemRequest
+            {
+                TableName = "TestTable",
+                Item = new Dictionary<string, AttributeValue>
+                {
+                    { "Id", new AttributeValue { S = "123" } },
+                    { "Name", new AttributeValue { S = "Test Item" } }
+                }
+            };
+
+            await _dynamoDbClient.PutItemAsync(putItemRequest);
+
+            var getItemRequest = new GetItemRequest
+            {
+                TableName = "TestTable",
+                Key = new Dictionary<string, AttributeValue>
+                {
+                    { "Id", new AttributeValue { S = "123" } }
+                }
+            };
+
+            var response = await _dynamoDbClient.GetItemAsync(getItemRequest);
+            Assert.IsTrue(response.Item.ContainsKey("Name"));
+
+            await _dynamoDbClient.DeleteItemAsync("TestTable", new Dictionary<string, AttributeValue>
+            {
+                { "Id", new AttributeValue { S = "123" } }
+            });
+        }
+    }
+}
